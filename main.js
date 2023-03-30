689ad646118f873f7c4fc032e1f48e0748f058c3
Nathan C Ilescas <nathan.ilescas@outlook.com>
2023-03-30 12:37:36 -0700

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+
+/* Slide Function */
+var image = 1;
+var slideImage = document.getElementById("Images")
+
+
+function plusSlide(num) {
+    if (image == 1 && num != -1){
+        image++;
+        slideImage.src = "./Images/Slide/slide_"+ image + ".png"
+        ChangeInfo(image)
+    }
+    if (image == 2 && num != 1) {
+        image--;
+        slideImage.src = "./Images/Slide/slide_"+ image + ".png"
+        ChangeInfo(image)
+
+    }
+}
+
+function ChangeInfo(imageNum) {
+    if(imageNum == 1 ) {
+        document.getElementById("GitHub_Info").style.display = "block"
+        document.getElementById("Experience_Info").style.display = "none"
+
+
+    }else if (imageNum == 2 ) {
+        document.getElementById("Experience_Info").style.display = "block"
+        document.getElementById("GitHub_Info").style.display = "none"
+
+    }
+
+}
