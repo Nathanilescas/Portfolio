b399b7701c46176ea2dd12a15334f23b31dbfee1
Nathan C Ilescas <nathan.ilescas@outlook.com>
2023-05-14 11:26:23 -0700

    Added two slides

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ var slideImage = document.getElementById("Images")
 
 
 function plusSlide(num) {
-    if (image == 1 && num != -1){
+    if ((image == 1 || image == 2 || image == 3) && num != -1){
         image++;
         slideImage.src = "./Images/Slide/slide_"+ image + ".png"
         ChangeInfo(image)
     }
-    if (image == 2 && num != 1) {
+    if ((image == 4 || image ==3 || image == 2) && num != 1) {
         image--;
         slideImage.src = "./Images/Slide/slide_"+ image + ".png"
         ChangeInfo(image)
@@ -21,13 +21,28 @@ function plusSlide(num) {
 function ChangeInfo(imageNum) {
     if(imageNum == 1 ) {
         document.getElementById("GitHub_Info").style.display = "block"
+        document.getElementById("CandUnity_Info").style.display = "none"
+        document.getElementById("CandUnreal_Info").style.display = "none"
         document.getElementById("Experience_Info").style.display = "none"
 
 
     }else if (imageNum == 2 ) {
         document.getElementById("Experience_Info").style.display = "block"
+        document.getElementById("CandUnity_Info").style.display = "none"
+        document.getElementById("CandUnreal_Info").style.display = "none"
         document.getElementById("GitHub_Info").style.display = "none"
 
+    }else if (imageNum == 3){
+        document.getElementById("Experience_Info").style.display = "none"
+        document.getElementById("CandUnity_Info").style.display = "block"
+        document.getElementById("CandUnreal_Info").style.display = "none"
+        document.getElementById("GitHub_Info").style.display = "none"
+    }
+    else if (imageNum ==4 ){
+        document.getElementById("Experience_Info").style.display = "none"
+        document.getElementById("CandUnity_Info").style.display = "none"
+        document.getElementById("CandUnreal_Info").style.display = "block"
+        document.getElementById("GitHub_Info").style.display = "none"
     }
 
 }
