091f953a86a691c5a4b287f5738c5a9d7202a275
nate_ilescas <125600285+Nathanilescas@users.noreply.github.com>
2024-01-02 13:10:54 -0800

    Delete JavaScript directory

--- JavaScript/HomePage.js
+++
@@ -1,6 +0,0 @@
-function OpenContact() {
-    document.getElementById("Contact").style.transform = "translateX(-75%)";
-}
-function CloseContact() {
-    document.getElementById("Contact").style.transform = "translateX(75%)";
-}
