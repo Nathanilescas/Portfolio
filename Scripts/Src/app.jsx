40f4741a7479c59d9943741c30e514cb18d817cb
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2024-01-16 18:43:56 -0800

    Finish: Navbar, Heading effect, Movement effect

--- Scripts/Src/app.jsx
+++ Scripts/Src/app.jsx
@@ -1,6 +1,28 @@
-const element = (
-    <div>
-        <h1>Hello World!!</h1>
-    </div>
-   );
-   ReactDOM.render(element, document.getElementById('test'));
+class HeaderText extends React.Component {
+    render() {
+        const textStyle = {
+            color: '#010101',
+            fontFamily: '"Jazz LET", fantasy', // Change the font later
+            fontSize: '1.5em',
+            letterSpacing: '.1em',
+            textAlign: 'center'
+        };
+
+        return(
+            <div>
+                <h1 style={textStyle}>Nathan's Portfolio</h1>
+            </div>
+        );
+    }
+}
+
+const Header = <HeaderText />
+
+   ReactDOM.render(Header, document.getElementById('Header-Text'));
+
+
+
+
+
+
+
